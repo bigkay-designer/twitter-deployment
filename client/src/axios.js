e58e9886284883b01d063323ec8a556c566e342b
khalid ibrahim <ibrahim.khalid4778@gmail.com>
2020-11-13 07:17:44 +0000

    added logout func

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // All url will start with the one below
 const instance = axios.create({
-    baseURL: "http://localhost:5000"
+    baseURL: "https://callmyneighbour.site"
 })
 
 
