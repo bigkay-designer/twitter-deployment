7340faf50077cb41e8aacf1efadc7eccbef4a1dc
khalid ibrahim <ibrahimkhalid478@gmail.com>
2021-08-22 05:23:50 +0100

    finished editing home css

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -1,211 +1,255 @@
-import React, {useState, useEffect} from 'react'
-import {Redirect} from 'react-router-dom'
-import axios from '../axios'
-import './css/feed.css'
-import {Button} from '@material-ui/core'
-import {StarOutlined, CropOriginal, InsertEmoticon, Schedule, GifOutlined, AccountCircle, Cancel} from '@material-ui/icons';
+import React, { useState, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
+import axios from '../axios';
+import './css/feed.css';
+import { Button } from '@material-ui/core';
+import {
+  StarOutlined,
+  CropOriginal,
+  InsertEmoticon,
+  Schedule,
+  GifOutlined,
+  AccountCircle,
+  Cancel,
+} from '@material-ui/icons';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import Posts from './Posts';
 
 function Feed() {
- 
-    const [userPost, setUserPost] = useState([])
-    const [postAdded, setPostAdded] = useState()
-    const [user, setUser] = useState({token: undefined, user: undefined})
-    const [tweetText, setTweetText] = useState('')
-    const [tweetImage, setTweetImage] = useState('')
-    const [linkTweetImage, setLinkTweetImage] = useState('')
-    const [showLinkBox, setShowLinkBox] = useState(false)
-    const [loggedout, setLoggedOut] = useState(false)
-    const [tweetBox, setTweetBox] = useState(false)
-    // const [file, setFile] = useState('')
-    // const [fileName, setFileName] = useState('choose file')
-    // const [uploadedFile, setUploadedFile] = useState({myFileName: '', filePath: ''})
+  const [userPost, setUserPost] = useState([]);
+  const [postAdded, setPostAdded] = useState();
+  const [user, setUser] = useState({ token: undefined, user: undefined });
+  const [tweetText, setTweetText] = useState('');
+  const [tweetImage, setTweetImage] = useState('');
+  const [linkTweetImage, setLinkTweetImage] = useState('');
+  const [showLinkBox, setShowLinkBox] = useState(false);
+  const [loggedout, setLoggedOut] = useState(false);
+  const [tweetBox, setTweetBox] = useState(false);
+  // const [file, setFile] = useState('')
+  // const [fileName, setFileName] = useState('choose file')
+  // const [uploadedFile, setUploadedFile] = useState({myFileName: '', filePath: ''})
 
+  useEffect(() => {
+    axios
+      .get('/api/post')
+      .then((data) => {
+        const newPost = data.data;
+        setUserPost(newPost);
+      })
+      .catch((err) => console.log(`error ${err}`));
+  }, [postAdded]);
 
-    useEffect(()=>{
-            axios.get("/api/post")
-            .then(data => {
-                const newPost = data.data
-                setUserPost(newPost)
-            })
-            .catch(err => console.log(`error ${err}`))
-    
-    }, [postAdded])
+  useEffect(() => {
+    axios
+      .get('/api', { headers: { 'auth-token': localStorage.getItem('token') } })
+      .then((res) => {
+        setUser(res.data);
+      })
+      .catch((err) => console.log(`error ${err}`));
+  }, []);
 
-    useEffect(()=>{
-            axios.get('/api', {headers: {"auth-token": localStorage.getItem("token")}
-            })
-            .then(res => {
-                setUser(res.data)
-            })
-            .catch(err => console.log(`error ${err}`))
-    
-    }, [])
+  const deletePostHandler = (e, dataId) => {
+    e.preventDefault();
+    axios
+      .delete(`/api/post/posts/${dataId}`)
+      .then((posts) => {})
+      .catch((err) => console.log(`error ${err}`));
+    axios.get('/api/post').then((data) => {
+      const newPost = data.data;
+      setPostAdded(newPost.length);
+    });
+  };
 
-    const deletePostHandler =(e, dataId) => {
-        e.preventDefault()
-        axios.delete(`/api/post/posts/${dataId}`)
-        .then(posts => {
-    
-        })
-        .catch(err => console.log(`error ${err}`))
-        axios.get("/api/post")
-        .then(data => {
-            const newPost = data.data
-            setPostAdded(newPost.length)
-        })
+  // const selectFileHandler = (e) =>{
+  //     setTweetImage(URL.createObjectURL(e.target.files[0]))
+  //     let handlerFile = e.target.files[0]
+  //     // setFile(handlerFile)
+  //     console.log(handlerFile)
+  // }
 
-    }
+  const tweetTextHandler = (e) => {
+    setTweetText(e.target.value);
+  };
 
-    // const selectFileHandler = (e) =>{
-    //     setTweetImage(URL.createObjectURL(e.target.files[0]))
-    //     let handlerFile = e.target.files[0]
-    //     // setFile(handlerFile)
-    //     console.log(handlerFile)
-    // }
+  const closeTweetImgHandler = () => {
+    setTweetImage('');
+  };
 
+  const submitHandler = async (e) => {
+    e.preventDefault();
 
-    const tweetTextHandler = (e)=>{
-        setTweetText(e.target.value)
-    }
+    // ********************************
+    // const formData = new FormData()
+    // formData.append('file', file)
+    // await axios.post('/api/post/posts', formData)
+    // .then(res => {
+    //     setUploadedFile({ myFileName: res.data.fileName, filePath: res.data.filePath});
+    // })
+    // .catch (err=>{
+    //     if(err.response.status === 500){
+    //         console.log(err)
+    //     }else{
+    //         console.log(err.response.data)
+    //     }
+    // })
+    // ********************************
+    const newPost = {
+      displayName: user.name,
+      username: user.username,
+      text: tweetText,
+      image: linkTweetImage,
+      avatar:
+        'https://polightafricafilms.com/wp-content/uploads/2019/07/avatar_afro_guy-512.png',
+      author: {
+        id: user.id,
+        name: user.name,
+      },
+    };
+    axios
+      .post('/api/post/posts', newPost)
+      .then((res) => {
+        // console.log(res)
+      })
+      .catch((err) => console.log(`error ${err.message}`));
 
-    const closeTweetImgHandler = () =>{
-        setTweetImage('')
-    }
+    axios
+      .get('/api/post')
+      .then((data) => {
+        const newPost = data.data;
+        setPostAdded(newPost.length);
+      })
+      .catch((err) => console.log(`error ${err}`));
+    setTweetImage('');
+    setTweetText('');
+    setLinkTweetImage('');
+    setShowLinkBox(false);
+    setTweetBox(false);
+  };
 
-    const submitHandler = async (e)=>{
-        e.preventDefault();
+  const tweetImageBg = {
+    background: `url(${tweetImage}) center center / cover no-repeat`,
+    width: '100%',
+    height: '300px',
+    marginBottom: '20px',
+  };
+  const logout = () => {
+    localStorage.removeItem('token');
+    setLoggedOut(true);
+  };
 
-        // ********************************
-        // const formData = new FormData()
-        // formData.append('file', file)
-        // await axios.post('/api/post/posts', formData)
-        // .then(res => {
-        //     setUploadedFile({ myFileName: res.data.fileName, filePath: res.data.filePath});
-        // })
-        // .catch (err=>{
-        //     if(err.response.status === 500){
-        //         console.log(err)
-        //     }else{
-        //         console.log(err.response.data)
-        //     }           
-        // })
-        // ********************************
-        const newPost =  {
-            displayName: user.name,
-            username: user.username,
-            text: tweetText,
-            image: linkTweetImage,
-            avatar: 'https://polightafricafilms.com/wp-content/uploads/2019/07/avatar_afro_guy-512.png',
-            author: {
-                id: user.id,
-                name: user.name
-            }
-        } 
-        axios.post("/api/post/posts", newPost)
-        .then(res => {
-            // console.log(res)
-        })
-        .catch(err => console.log(`error ${err.message}`))
+  const tweetBoxHandler = (e) => {
+    e.preventDefault();
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+      setTweetBox(true);
+    }, 10);
+  };
+  const redirectHomeHandler = (e) => {
+    e.preventDefault();
+    setTweetBox(false);
+  };
 
-        axios.get("/api/post")
-        .then(data => {
-            const newPost = data.data
-            setPostAdded(newPost.length)
-        })
-        .catch(err => console.log(`error ${err}`))
-        setTweetImage('')
-        setTweetText('')
-        setLinkTweetImage('')
-        setShowLinkBox(false)
-        setTweetBox(false)
-    }
-   
-    const tweetImageBg = {
-        background: `url(${tweetImage}) center center / cover no-repeat`,
-        width: '100%',
-        height: '300px',
-        marginBottom: '20px'
-    }
-    const logout = ()=>{
-        localStorage.removeItem("token")
-        setLoggedOut(true)
-    }
-    
-    const tweetBoxHandler = (e) => {
-        e.preventDefault()
-        setTimeout(() => {
-            window.scrollTo(0, 0)
-            setTweetBox(true) 
-        }, 10)
-    }
-    const redirectHomeHandler = (e)=>{
-        e.preventDefault()
-        setTweetBox(false)
-    }
-
-    
-    return (
-        <div className="feed">
-            {loggedout ? <Redirect to='/' /> : null }
-            <div className="feed__container">
-                <form onSubmit={submitHandler}>
-                    <div className="feed__title">
-                        <div className="feed__user">
-                            <Button className="feed__user__btn" onClick={logout} variant="outlined" >logout</Button>
-                        </div>
-                        <h1>home</h1>
-                        <StarOutlined className="feed__title--icon" />
-                    </div>
-                    <div className={`feed__tweet ${tweetBox? "feed__tweet__popup": null}`}>
-                        <div className="feed__tweet__cancle">
-                            <h3 onClick={redirectHomeHandler}>Cancel</h3>
-                        </div>
-                        <div className="feed__tweet__title">
-                            <AccountCircle className="tweet__icon" />
-                            <textarea className="feed__input" type="text" value={tweetText} placeholder="what's happeing?" onChange={tweetTextHandler}  cols="30" rows="3"></textarea>
-                        </div>
-                        {tweetImage ? <div style={tweetImageBg} className="feed__imageFile">
-                                 <span onClick={closeTweetImgHandler}> <Cancel className="feed__imageFile--close" /> </span> 
-                             </div> : null 
-                        }
-                        <div className="feed__tweet__attachment">
-                            <input className={`feed__tweet__input  ${showLinkBox && "feed__tweet__input--show"}`} type="text" id="file" placeholder="paste the URL here" onChange={(e)=> setLinkTweetImage(e.target.value)} value={linkTweetImage} />
-                            <label htmlFor="file" onClick={() => setShowLinkBox(true)}><CropOriginal className="tweet__attachment__icon" /> </label>
-
-                            <GifOutlined className="tweet__attachment__icon gif" />
+  return (
+    <div className="feed">
+      {loggedout ? <Redirect to="/" /> : null}
+      <div className="feed__container">
+        <form onSubmit={submitHandler}>
+          <div className="feed__title">
+            <div className="feed__user">
+              <Button
+                className="feed__user__btn"
+                onClick={logout}
+                variant="outlined"
+              >
+                logout
+              </Button>
+            </div>
+            <h1>home</h1>
+            <StarOutlined className="feed__title--icon" />
+          </div>
+          <div
+            className={`feed__tweet ${tweetBox ? 'feed__tweet__popup' : null}`}
+          >
+            <div className="feed__tweet__cancle">
+              <h3 onClick={redirectHomeHandler}>Cancel</h3>
+            </div>
+            <div className="feed__tweet__title">
+              <AccountCircle className="tweet__icon" />
+              <textarea
+                className="feed__input"
+                type="text"
+                value={tweetText}
+                placeholder="what's happeing?"
+                onChange={tweetTextHandler}
+                cols="30"
+                rows="3"
+              ></textarea>
+            </div>
+            {tweetImage ? (
+              <div style={tweetImageBg} className="feed__imageFile">
+                <span onClick={closeTweetImgHandler}>
+                  {' '}
+                  <Cancel className="feed__imageFile--close" />{' '}
+                </span>
+              </div>
+            ) : null}
+            <div className="feed__tweet__attachment">
+              <input
+                className={`feed__tweet__input  ${
+                  showLinkBox && 'feed__tweet__input--show'
+                }`}
+                type="text"
+                id="file"
+                placeholder="paste the URL here"
+                onChange={(e) => setLinkTweetImage(e.target.value)}
+                value={linkTweetImage}
+              />
+              <label htmlFor="file" onClick={() => setShowLinkBox(true)}>
+                <CropOriginal className="tweet__attachment__icon" />{' '}
+              </label>
+            </div>
+            <div className="feed__tweet__attachment__icons">
+              {/* <GifOutlined` className="tweet__attachment__icon gif" />
                             <InsertEmoticon className="tweet__attachment__icon" />
-                            <Schedule className="tweet__attachment__icon" /> 
-                        </div>
-                        <div className="feed__tweet__btn">
-                            <Button className="feed__tweet__btn" variant="outlined" type="submit"  >tweet</Button>
-                        </div>
-                    </div>
-                
-                    {/* ************************************************************************** */}
-                    <div className="feed__posts">
-                        {userPost.map(data=>(
-                            <Posts 
-                                keys={data._id}
-                                displayName={data.displayName}
-                                username={data.username}
-                                text={data.text}
-                                image={data.image}
-                                verified={data.verified}
-                                avatar={data.avatar}
-                                deletePost = {(e)=> deletePostHandler(e, data._id)}
-                                currentuser = {user.username}
-                            />
-                        ))}
-                    </div>
-                </form>
+                            <Schedule cl`assName="tweet__attachment__icon" />  */}
             </div>
-            <div className="add__tweet">
-                <Button className="feed__add__tweet__btn" onClick={tweetBoxHandler}><AddCircleOutlineIcon className="feed__add__icon" /></Button>                
+
+            <div className="feed__tweet__btn">
+              <Button
+                className="feed__tweet__btn"
+                variant="outlined"
+                type="submit"
+              >
+                tweet
+              </Button>
             </div>
-        </div>
-    )
+          </div>
+
+          {/* ************************************************************************** */}
+          <div className="feed__posts">
+            {userPost.map((data) => (
+              <Posts
+                keys={data._id}
+                displayName={data.displayName}
+                username={data.username}
+                text={data.text}
+                image={data.image}
+                verified={data.verified}
+                avatar={data.avatar}
+                deletePost={(e) => deletePostHandler(e, data._id)}
+                currentuser={user.username}
+              />
+            ))}
+          </div>
+        </form>
+      </div>
+      <div className="add__tweet">
+        <Button className="feed__add__tweet__btn" onClick={tweetBoxHandler}>
+          <AddCircleOutlineIcon className="feed__add__icon" />
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default Feed
+export default Feed;
--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,67 +1,68 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import { Avatar } from '@material-ui/core';
 import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
-import {ChatBubbleOutline,Favorite,FavoriteBorder, DeleteOutline , Repeat} from '@material-ui/icons'
-import './css/posts.css'
-function Posts( {displayName,username,verified,text,image,avatar, keys, deletePost, currentuser}) {
-    
-    const [isPostLiked, setIsPostLiked] = useState(false)
-    const [postLikes, setPostLikes] = useState('')
-
-    let likedUsers = ['']
-
-    const likePostHandler = (e) => {
-        e.preventDefault();
-
-        likedUsers.map(likes=>{
-                
-            if(likes === currentuser){
-                const indexOfUser = likedUsers.indexOf(currentuser)
-                likedUsers.splice(indexOfUser,1)
-                console.log('yay')
-                return setIsPostLiked(false)
-            }else{
-                likedUsers.splice(0,0, currentuser)
-                setPostLikes(likedUsers.length)
-                return setIsPostLiked(true)
-            }
-        })
-        console.log(likedUsers)
-    }
-    return (
-        <div className="posts"  key={keys}>
-            <div className="posts__avatar">
-                <Avatar src={avatar} />
-            </div>
-            <div className="posts__body">
-                <div className="posts__header">
-                    <div className="posts__headerText">
-                        <h3>{displayName} </h3>
-                        <span className={`${!verified && "posts__verified--none" }`}> <VerifiedUserIcon className="posts__verified" /> </span>  
-                        <span className="span">@{username} </span>
-                        
-                    </div>
-                    <div className="posts__description">
-                        <span> {text} </span>
-                    </div>
-                </div>
-                {image ? <img src={image} alt="gif"/> : null }
-
-                <div className="posts__icons">
-                    <ChatBubbleOutline />
-                    <Repeat />
-                    {isPostLiked ? 
-                       <span onClick={likePostHandler}> <Favorite className='heartRed' /> </span> 
-                    :  <span onClick={likePostHandler}> <FavoriteBorder /></span>
-                    }
-                    
-                    {currentuser === username ?  
-                        <span onClick={deletePost}> <DeleteOutline /> </span>
-                    :null }
-                </div>
-            </div>
+import {
+  ChatBubbleOutline,
+  FavoriteBorder,
+  DeleteOutline,
+  Repeat,
+  Favorite,
+} from '@material-ui/icons';
+import './css/posts.css';
+function Posts({
+  displayName,
+  username,
+  verified,
+  text,
+  image,
+  avatar,
+  keys,
+  deletePost,
+  currentuser,
+}) {
+  const [isPostLiked, setIsPostLiked] = useState(false);
+  const [postLiked, setPostLiked] = useState(false);
+  return (
+    <div className="posts" key={keys}>
+      <div className="posts__avatar">
+        <Avatar src={avatar} />
+      </div>
+      <div className="posts__body">
+        <div className="posts__header">
+          <div className="posts__headerText">
+            <h3>{displayName} </h3>
+            <span className={`${!verified && 'posts__verified--none'}`}>
+              {' '}
+              <VerifiedUserIcon className="posts__verified" />{' '}
+            </span>
+            <span className="span">@{username} </span>
+          </div>
+          <div className="posts__description">
+            <span> {text} </span>
+          </div>
         </div>
-    )
+        {image ? <img src={image} alt="gif" /> : null}
+
+        <div className="posts__icons">
+          <ChatBubbleOutline className="showSidebar__notAvailable" />
+          <Repeat className="showSidebar__notAvailable" />
+          <span onClick={() => setPostLiked(!postLiked)}>
+            {postLiked ? (
+              <Favorite className={`${postLiked && 'heartRed'}`} />
+            ) : (
+              <FavoriteBorder />
+            )}
+          </span>
+          {currentuser === username ? (
+            <span onClick={deletePost}>
+              {' '}
+              <DeleteOutline />{' '}
+            </span>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Posts
+export default Posts;
